1e7c482245f145252e37eeaad4c0b1bb6e6e0d91
sourabh <sourabh@sumologic.com>
2019-05-16 21:33:47 +0530

    Third Commit

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -1,35 +1,33 @@
 import org.junit.Test;
 
-import static sun.misc.PostVMInitHook.run;
-
 public class SecondTest extends Base{
     @Test
     public void test1() {
-        run();
+        BaseTestClassMethod();
     }
 
     @Test
     public void test2() {
-        run();
+        BaseTestClassMethod();
     }
 
     @Test
     public void test3() {
-        run();
+        BaseTestClassMethod();
     }
 
     @Test
     public void test4() {
-        run();
+        BaseTestClassMethod();
     }
 
     @Test
     public void test5() {
-        run();
+        BaseTestClassMethod();
     }
 
     @Test
     public void test6() {
-        run();
+        BaseTestClassMethod();
     }
 }
